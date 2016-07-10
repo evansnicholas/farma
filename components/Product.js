68d509ac9bb7f5dcf110640e08ec50d6a11ff9f0
Nick Evans <evansnicholas@hotmail.com>
2016-07-10 11:57:08 +0200

    Improving display of products and product details

--- components/Product.js
+++ components/Product.js
@@ -6,12 +6,16 @@ const Product = ({
 }) => {
 
   const prodDetailsClass = (product.showDetails) ? "show" : "hidden";
+  const triangleDirection = (prodDetailsClass === "show") ? "left" : "bottom";
 
   return (
-    <div className="product" onClick={() => onToggleProductVisibility(product.id)}>
-      <p>{product.name}</p>
-      <div className={prodDetailsClass}>
-        <p><strong>{"Description:"}</strong>{"This is a product description"}</p>
+    <div className="product">
+      <div className={`product-name product-details-${prodDetailsClass}`} onClick={() => onToggleProductVisibility(product.id)}>
+        <span className={`pull-right glyphicon glyphicon-triangle-${triangleDirection}`} aria-hidden="true"></span>
+        <p>{product.name}</p>
+      </div>
+      <div className={`product-details ${prodDetailsClass}`}>
+        <p><strong>{"Description: "}</strong>{"This is a product description"}</p>
       </div>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
     './index',
     './farma-css/index.less'
   ],
